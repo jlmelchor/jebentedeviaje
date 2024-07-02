b8e4f33bdbc270f7c0076f8fe4b639c067236a85
LOPEZ MELCHOR Jennifer <jennifer.lopez@soprasteria.com>
2024-07-02 14:18:53 +0200

    Jebentedeviaje project -> back-end and front-end set up

---
+++ jebentedeviaje-backend/src/main/java/com/project/jebentedeviaje_backend/JebentedeviajeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.jebentedeviaje_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JebentedeviajeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JebentedeviajeBackendApplication.class, args);
+	}
+
+}
---
+++ jebentedeviaje-backend/src/test/java/com/project/jebentedeviaje_backend/JebentedeviajeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jebentedeviaje_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JebentedeviajeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
